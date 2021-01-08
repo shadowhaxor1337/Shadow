8c6fc25542b2c7abd004c1723a642072d1e223fa
shadowhaxor1337 <76968702+shadowhaxor1337@users.noreply.github.com>
2021-01-08 10:51:24 -0600

    Update Shadow.py

--- Shadow.py
+++ Shadow.py
@@ -112,7 +112,7 @@ print  """\033[1;91m
                      ▇▇▇◣╲▇╱◢▇▇▇
                      ▇▇▇▇◣▇◢▇▇▇▇
                    
-\033[1;96mAuthor  \033[1;93m: \033[1;92mShadow
+\033[1;96mAuthor  \033[1;93m: \033[1;92m Shadow
 \033[1;96mInstagram \033[1;93m: \033[1;92mplayboyalex214
 \033[1;96mFacebook  \033[1;93m: \033[1;92m alex avila
 \033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/shadowhaxor1337/Shadow
